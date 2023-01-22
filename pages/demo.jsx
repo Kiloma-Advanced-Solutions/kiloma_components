a8148ed7bd03062b48cb5d6ec5be284c50c9fd26
EkingIsrael <eyalbenjamin@gmail.com>
2023-01-22 10:19:40 +0200

    First demo commit

---
+++ pages/demo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Demo() {
+  return (
+    <div>
+      <h1>Demo</h1>
+      <h2>
+        This is our first demo page
+      </h2>
+    </div>
+  );
+}
