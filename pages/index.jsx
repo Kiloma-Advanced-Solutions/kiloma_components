b9b31b96e7fd6731c5099122a21398f57074aaaa
RazSherf <razsherf@gmail.com>
2023-01-24 11:11:28 +0200

    adding another divs for the css

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,14 +5,18 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.img_title}>
-        <Image src='/images/logo.svg' width={100} height={100} />
-        <p>Kiloma Components</p>
+      <div className={styles.inside_div}>
+        <div className={styles.img_title}>
+          <Image src='/images/logo.svg' width={100} height={100} />
+          <p>Kiloma Components</p>
+        </div>
+        <div className={styles.button_div}>
+          <button type='button' className={styles.button}>
+            Browse
+          </button>
+        </div>
       </div>
-      <div className={styles.button_img}>
-        <button type='button' className={styles.button}>
-          Browse
-        </button>
+      <div className={styles.img_div}>
         <Image
           src='\Images\library.svg'
           width={1000}
