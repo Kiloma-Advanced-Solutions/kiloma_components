d63839c140263c9dc310ec0c035d83456a078df0
RazSherf <razsherf@gmail.com>
2023-01-31 13:40:35 +0200

    fixing eslint issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,26 @@
-/* eslint-disable jsx-quotes */
 import React from 'react';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const logoImg = '/images/logo.svg';
   return (
     <div className={styles.container}>
       <div className={styles.inside_div}>
         <div className={styles.img_title}>
-          <Image src='/images/logo.svg' width={100} height={100} />
+          <Image src={logoImg} width={100} height={100} />
           <p>Kiloma Components</p>
         </div>
         <div className={styles.button_div}>
-          <button type='button' className={styles.button}>
+          <button type="button" className={styles.button}>
             Browse
           </button>
         </div>
       </div>
       <div className={styles.img_div}>
         <Image
-          src='\Images\library.svg'
-          fill='true'
+          src="\Images\library.svg"
+          fill="true"
           className={styles.lower_img}
         />
       </div>
