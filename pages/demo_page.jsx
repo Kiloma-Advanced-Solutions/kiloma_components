db6fecf34c029278e0ff33c8a6684cc67936c187
RazSherf <razsherf@gmail.com>
2023-02-15 10:45:23 +0200

    missing ;

--- pages/demo_page.jsx
+++ pages/demo_page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/Demo_page.module.css'
+import styles from '../styles/Demo_page.module.css';
 
 export default function demoPage() {
   return (
