c24683a2bf751eaf4672027046b34c156327d325
RazSherf <razsherf@gmail.com>
2023-02-21 12:19:47 +0200

    npm run build now running

--- pages/demo.jsx
+++ pages/demo.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import ButtonComponents from '../components/button/Button';
 
 export default function Demo() {
-  const alertOnClick = () => {
-    alert('Working>');
-  };
   return (
     <div>
       <ButtonComponents
         ButtonLabel="Button"
         ButtonTheme="round"
-        Onclick={alertOnClick}
       />
     </div>
   );
