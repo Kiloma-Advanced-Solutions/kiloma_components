d499db96460dceceda5c1ee0146eae89d50e9751
RazSherf <razsherf@gmail.com>
2023-02-27 11:10:56 +0200

    components page fixed and example now added

--- pages/components.jsx
+++ pages/components.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
 import styles from '../styles/components.module.css';
+import ButtonComponents from '../components/button/ButtonComponent ';
 import CheckBox from '../components/Checkbox2/Checkbox2';
 
 export default function components() {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        <h1>Components</h1>
+      <h1 className={styles.title}>Components</h1>
+      <div className={styles.input_div}>
+        <h2>Input Components</h2>
+        <ol>
+          <li>
+            {' '}
+            <CheckBox
+              CheckBoxBackGroundColor="#24b655"
+              CheckBoxHeight="15"
+              CheckBoxTitle="Hello"
+              CheckBoxWidth="15"
+              IdForTheLabel="1"
+              IsChecked=""
+              IsRequired={false}
+              LabelValue="1"
+              ShouldDisplayLabel
+              labelColor="#d20808"
+            />
+
+          </li>
+        </ol>
       </div>
-      <div className={styles.input_title}>
-        <h2 className={styles.sub_title}>Input Components</h2>
-      </div>
-      <div className={styles.input_components}>
-        <div className={styles.examples}>
-          <ol>
-            <li>
-              <div className={styles.examples}>
-                <p> Checkbox - </p>
-                <CheckBox
-                  CheckBoxTitle="Agree"
-                  IsChecked=""
-                  IsRequired={false}
-                  ShouldDisplayLabel
-                  labelColor="#19d443"
-                  OnChange={() => {}}
-                  CheckBoxWidth="25"
-                  CheckBoxHeight="15"
-                  LabelFontSize="20"
-                />
-              </div>
-            </li>
-          </ol>
-        </div>
-        <h2 className={styles.sub_title}> UI Components</h2>
+      <div className={styles.ui_div}>
+        <h2>UI Components</h2>
+        <ol>
+          <li>
+            <ButtonComponents
+              ButtonLabel="Button"
+              ButtonTheme="dark"
+              IsRound={false}
+              IsShaded
+            />
+          </li>
+        </ol>
       </div>
     </div>
   );
