3fd8a02d90b5ee28995bd0ecdffbaf9d95e062c1
RazSherf <razsherf@gmail.com>
2023-02-27 14:54:11 +0200

    Components page edit

--- pages/components.jsx
+++ pages/components.jsx
@@ -6,38 +6,37 @@ import CheckBox from '../components/Checkbox2/Checkbox2';
 export default function components() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Components</h1>
+      <div className={styles.title}>
+        <h1>Components</h1>
+      </div>
       <div className={styles.input_div}>
-        <h2>Input Components</h2>
-        <ol>
-          <li>
-            {' '}
-            <CheckBox
-              CheckBoxBackGroundColor="#24b655"
-              CheckBoxHeight="15"
-              CheckBoxTitle="Hello"
-              CheckBoxWidth="15"
-              IdForTheLabel="1"
-              IsChecked=""
-              IsRequired={false}
-              LabelValue="1"
-              ShouldDisplayLabel
-              labelColor="#d20808"
-            />
+        <h2 className={styles.h2}>Input Components</h2>
+        <div className={styles.examples}>
+          <p>1.CheckBox Component - </p>
+          <CheckBox
+            CheckBoxBackGroundColor="#24b655"
+            CheckBoxHeight="15"
+            CheckBoxTitle="Hello"
+            CheckBoxWidth="15"
+            IdForTheLabel="1"
+            IsChecked=""
+            IsRequired={false}
+            LabelValue="1"
+            ShouldDisplayLabel
+            labelColor="#d20808"
+          />
 
-          </li>
-        </ol>
+        </div>
       </div>
       <div className={styles.ui_div}>
-        <h2>UI Components</h2>
-        <ol>
-          <li>
-            <ButtonComponents
-              ButtonLabel="Click Here"
-              IsRound={false}
-            />
-          </li>
-        </ol>
+        <h2 className={styles.h2}>UI Components</h2>
+        <div className={styles.examples}>
+          <p>1.Button Component - </p>
+          <ButtonComponents
+            ButtonLabel="Click Here"
+            IsRound={false}
+          />
+        </div>
       </div>
     </div>
   );
