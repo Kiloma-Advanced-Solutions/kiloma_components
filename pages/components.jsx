5230331823361fea1bf47a7804329c525fcc6d69
RazSherf <101274250+RazSherf@users.noreply.github.com>
2023-02-28 15:04:54 +0200

    fixing the component rout

--- pages/components.jsx
+++ pages/components.jsx
@@ -8,33 +8,42 @@ export default function components() {
     <div className={styles.container}>
       <h1 className={styles.title}>Components</h1>
       <div className={styles.input_div}>
-        <h2>Input Components</h2>
-        <ol>
-          <li>
-            {' '}
-            <CheckBox
-              CheckBoxHeight="15"
-              CheckBoxTitle="Agree  ✅"
-              CheckBoxWidth="15"
-              IdForTheLabel="id"
-              IsChecked=""
-              IsRequired={false}
-              LabelColor="#88cb7e"
-              ShouldDisplayLabel
-            />
-          </li>
-        </ol>
+        <h2 className={styles.H2}>Input Components </h2>
+        <div className={styles.components_examples}>
+          <ol className={styles.ol1}>
+            <li>
+              <div className={styles.components_examples}>
+                <p className={styles.p1}> CheckBox- </p>
+                <CheckBox
+                  CheckBoxHeight="15"
+                  CheckBoxTitle="Agree ✅"
+                  CheckBoxWidth="15"
+                  IdForTheLabel="checkbox"
+                  IsChecked=""
+                  IsRequired={false}
+                  ShouldDisplayLabel
+                  labelColor="#1f7dda"
+                />
+              </div>
+            </li>
+          </ol>
+        </div>
       </div>
       <div className={styles.ui_div}>
-        <h2>UI Components</h2>
-        <ol>
-          <li>
-            <ButtonComponents
-              ButtonLabel="Click Here"
-              IsRound={false}
-            />
-          </li>
-        </ol>
+        <h2 className={styles.H2}>UI Components</h2>
+        <div className={styles.components_examples}>
+          <ol className={styles.ol1}>
+            <li>
+              <div className={styles.components_examples}>
+                <p className={styles.p1}> Button- </p>
+                <ButtonComponents
+                  ButtonLabel="Click Here"
+                  IsRound={false}
+                />
+              </div>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );
