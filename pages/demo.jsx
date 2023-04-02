c59082288d77cc015a5e4b1fb12be371121c9b86
RazSherf <101274250+RazSherf@users.noreply.github.com>
2023-04-02 15:47:14 +0300

    fixed the rows and colm props

--- pages/demo.jsx
+++ pages/demo.jsx
@@ -4,13 +4,20 @@ import Textarea from '../components/TextArea/TextAreaComponent';
 export default function Demo() {
   return (
     <div>
+
       <Textarea
-        AutoComplete={false}
-        Column={50}
-        Id="1"
-        IsRequired
-        Label="sdfsdf"
-        Name="1"
+        AutoComplete
+        Column={40}
+        Id=""
+        Label="English Example"
+        LabelColor="blue"
+        LabelFontFamily="Helvetica, Arial sans-serif"
+        LabelPlace="Left"
+        Name=""
+        PlaceHolder="Enter Your text here..."
+        Row={20}
+        ShadowColor=""
+        TextFontFamily="Helvetica, Arial sans-serif"
       />
     </div>
   );
